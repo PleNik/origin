6fad8566b8eae8c31a7956525a122dee32b9bb9f
PleNik <pleshkovn69@gmail.com>
2023-12-23 15:24:35 +0500

    Task2.3 added

---
+++ Lesson 2/Task2.3/Task2.3/Task2.3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include<string>
+#include<Windows.h>
+
+struct Address
+{
+    Address(const std::string& city, const std::string& street, int houseNumber, int apartamentNumber, int index)
+    {
+        this->city = city;
+        this->street = street;
+        this->houseNumber = houseNumber;
+        this->apartamentNumber = apartamentNumber;
+        this->index = index;
+    }
+
+    std::string city;
+    std::string street;
+    int houseNumber = 0;
+    int apartamentNumber = 0;
+    int index = 0;
+};
+
+void printAddress(Address& address)
+{
+    std::cout << "Город: " << address.city << std::endl;
+    std::cout << "Улица: " << address.street << std::endl;
+    std::cout <<"Номер дома: " << address.houseNumber << std::endl;
+    std::cout << "Номер квартиры: " << address.apartamentNumber << std::endl;
+    std::cout << "Индекс: " << address.index << std::endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Address address1("Москва", "Арбат", 12, 8, 123456);
+    Address address2("Ижевск", "Пушкина", 59, 143, 953769);
+    
+    printAddress(address1);
+    std::cout << "\n";
+
+    printAddress(address2);
+
+    return 0;
+}
