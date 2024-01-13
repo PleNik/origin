644df21540e636a09e27b0e84f1350dcbd7c3067
PleNik <pleshkovn69@gmail.com>
2024-01-13 14:48:26 +0500

    Task6.1 added

---
+++ Lesson6/Task6.1/Task6.1/Task6.1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include"math.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	double a{}, b{};
+	std::cout << "Введите первое число: ";
+	std::cin >> a;
+	std::cout << "Введите второе число: ";
+	std::cin >> b;
+	
+	int userInput{};
+	std::cout << "Введите операцию (1- сложение, 2 - вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень):";
+	std::cin >> userInput;
+
+	switch (userInput)
+	{
+	case 1:
+		std::cout << a << "+" << b << "=" << addition(a, b) << std::endl;
+		break;
+	case 2:
+		std::cout << a << "-" << b << "=" << subtraction(a, b) << std::endl;
+		break;
+	case 3:
+		std::cout << a << "*" << b << "=" << multiplication(a, b) << std::endl;
+		break;
+	case 4:
+		if (!b) {
+			std::cout << "Неверный ввод, деление на ноль" << std::endl;
+			exit;
+		}
+		std::cout << a << "/" << b << "=" << division(a, b) << std::endl;
+		break;
+	case 5:
+		std::cout << a << " в степени " << b << " = " << exponentiation(a, b) << std::endl;
+		break;
+	}
+
+	return 0;
+}
---
+++ Lesson6/Task6.1/Task6.1/math.cpp
@@ -0,0 +1,25 @@
+#include "math.h"
+
+int addition(int a, int b) {
+    return a + b;
+}
+
+int subtraction(int a, int b) {
+    return a - b;
+}
+
+int multiplication(int a, int b) {
+    return a * b;
+}
+
+int division(int a, int b) {
+    return a / b;
+}
+
+int exponentiation(int a, int b) {
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
+    }
+    return result;
+}
