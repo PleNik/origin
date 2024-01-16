e00c0e309c0b610e4c40f57b3e6ccfefa008325e
PleNik <pleshkovn69@gmail.com>
2024-01-16 19:36:48 +0500

    Task7.2 added

---
+++ Lesson7/Task7.2/Task7.2/Task7.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#define SUB(a,b) ((a)-(b))
+
+int main() {
+   
+    int a = 6;
+    int b = 5;
+    int c = 2;
+
+    int differ = SUB(a, b);
+    std::cout << differ << std::endl;
+
+    std::cout << differ * c << std::endl;
+
+    int sum = b + c;
+    differ = SUB(a, sum);
+    std::cout << differ * c << std::endl;
+
+    return 0;
+}
