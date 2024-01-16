2c31554ec01822a62b2d24c4c4597dab4dc8033c
PleNik <pleshkovn69@gmail.com>
2024-01-16 19:21:17 +0500

    Task7.1 added

---
+++ Lesson7/Task7.1/Task7.1/Task7.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+int add(int x, int y) {
+	return x + y;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+#ifdef MODE
+#if MODE==0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE==1
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	int a, b;
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+	std::cout << "Введите число 2: ";
+	std::cin >> b;
+
+	std::cout << "Результат сложения: " << add(a, b) << std::endl;
+#else
+	std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+#endif
+#else
+#error Необходимо определить MODE;
+#endif // MODE
+
+
+	return 0;
+}
