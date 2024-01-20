bcf803d61d21b6fe87a797a399d8f306fce25e1a
PleNik <pleshkovn69@gmail.com>
2024-01-20 11:02:57 +0500

    Task8.1 added

---
+++ Lessson8/Task8.1/Task8.1/Task8.1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include<string>
+#include<Windows.h>
+
+int function(std::string str, int fobidden_length);
+
+int main() {
+   
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int fobidden_length{};
+    std::cout << "Введите запретную длину: ";
+    std::cin >> fobidden_length;
+
+    std::string str;
+    
+
+    while (true) {
+
+        std::cout << "Введите слово: ";
+        std::cin >> str;
+
+        try {
+
+            std::cout << "Длина слова " << str << " равна " << function(str, fobidden_length) << std::endl;
+        }
+        catch (const char* bad_length) {
+            std::cout << bad_length << std::endl;
+            break;
+        }
+
+    }
+
+    return 0;
+}
+
+int function(std::string str, int fobidden_length)
+{
+    if (str.length() != fobidden_length) {
+
+        return str.length();
+    }
+    
+    throw "Вы ввели слово запретной длины! До свидания";
+}
