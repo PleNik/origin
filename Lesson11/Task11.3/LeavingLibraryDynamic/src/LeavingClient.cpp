b7bf73365746a1da70f9136e541a2816cc4550dd
PleNik <pleshkovn69@gmail.com>
2024-02-04 07:08:53 +0500

    Task11.3 added

---
+++ Lesson11/Task11.3/LeavingLibraryDynamic/src/LeavingClient.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<Windows.h>
+
+#include"DynamicLeaving.h"
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите имя: ";
+	std::string name;
+	std::cin >> name;
+
+	DynamicLeaving::Leaver lvr;
+
+	std::cout << lvr.leave(name) << std::endl;
+
+	system("pause");
+
+	return 0;
+}
