0866d7754b8b80b13e6b4c1c98040579b4cd0a12
PleNik <pleshkovn69@gmail.com>
2024-04-07 09:02:36 +0500

    Task5.1 added

---
+++ AdvancedProgramming/Task5.1/Task5.1/Task5.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<vector>
+
+template<class T>
+void squaring(T& a) {
+    a *= a;
+}
+
+template<>
+void squaring(std::vector<int>& v) {
+    for (auto& i : v) {
+        i *= i;
+    }
+}
+
+int main() {
+    int x = 4;
+    std::cout << "[IN]: " << x << std::endl;
+    squaring(x);
+    std::cout << "[OUT]: " << x << std::endl;
+
+    std::vector<int> vect {-1, 4, 8};
+    std::cout << "[IN]: ";
+    for (const auto i : vect)
+        std::cout << i << ", ";
+    std::cout << std::endl;
+
+    squaring(vect);
+    std::cout << "[OUT]: ";
+    for (const auto& i : vect)
+        std::cout << i << ", ";
+
+}
