954d74475520b254e0d137ccf419e7f3ba2cb547
PleNik <pleshkovn69@gmail.com>
2024-04-19 17:31:32 +0500

    Task7.2 added

---
+++ AdvancedProgramming/Task7.2/Task7.2/Task7.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<set>
+
+int main() {
+    std::cout << "[IN]:" << std::endl;
+    int size = 0;
+    int value = 0;
+    std::set<int> my_set;
+
+    std::cin >> size;
+    for (int i = 0; i < size; i++) {
+        std::cin >> value;
+        my_set.insert(value);
+    }
+
+    std::cout << "[OUT]:"<<std::endl;
+
+   for (auto itr = my_set.rbegin(); itr != my_set.rend(); itr++)
+        std::cout << *itr << std::endl;
+}
