b5d4803b1223dd05c50fab3c8fd22f7a979166ba
PleNik <pleshkovn69@gmail.com>
2024-06-06 21:53:32 +0500

    DbORM_classes modified

--- Working with databases/Task6/DbORM_classes/main.cpp
+++ Working with databases/Task6/DbORM_classes/main.cpp
@@ -10,6 +10,11 @@ class Stock;
 class Shop;
 class Sale;
 
+class Book;
+class Stock;
+class Shop;
+class Sale;
+
 class Publisher {
 public:
 	std::string name;
@@ -18,6 +23,7 @@ public:
 	template<typename Action>
 	void persist(Action& a) {
 		Wt::Dbo::field(a, name, "name");
+		Wt::Dbo::hasMany(a, books, Wt::Dbo::ManyToOne, "publisher");
 	}
 
 };
@@ -26,12 +32,13 @@ class Book {
 public:
 	std::string title;
 	Wt::Dbo::ptr<Publisher> publisher;
-
+	Wt::Dbo::collection<Wt::Dbo::ptr<Stock>> stocks;
 
 	template<typename Action>
 	void persist(Action& a) {
 		Wt::Dbo::field(a, title, "title");
 		Wt::Dbo::belongsTo(a, publisher, "publisher");
+		Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "book");
 	}
 };
 
@@ -40,12 +47,14 @@ public:
 	int count{};
 	Wt::Dbo::ptr<Shop> shops;
 	Wt::Dbo::ptr<Book> books;
+	Wt::Dbo::collection<Wt::Dbo::ptr<Sale>> sales;
 
 	template<typename Action>
 	void persist(Action& a) {
 		Wt::Dbo::field(a, count, "count");
 		Wt::Dbo::belongsTo(a, shops, "shop");
 		Wt::Dbo::belongsTo(a, books, "book");
+		Wt::Dbo::hasMany(a, sales, Wt::Dbo::ManyToOne, "stock");
 	}
 };
 
@@ -57,20 +66,21 @@ public:
 	template<typename Action>
 	void persist(Action& a) {
 		Wt::Dbo::field(a, name, "name");
+		Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "shop");
 	}
 };
 
 class Sale {
 public:
 	double price{};
-	std::string data_sale;
+	std::string date_sale;
 	int count{};
 	Wt::Dbo::ptr<Stock> stocks;
 
 	template<typename Action>
 	void persist(Action& a) {
 		Wt::Dbo::field(a, price, "price");
-		Wt::Dbo::field(a, data_sale, "data_sale");
+		Wt::Dbo::field(a, date_sale, "date_sale");
 		Wt::Dbo::field(a, count, "count");
 		Wt::Dbo::belongsTo(a, stocks, "stock");
 	}
@@ -78,37 +88,5 @@ public:
 
 int main() {
 
-	//setlocale(LC_ALL, "ru");
-
-	SetConsoleCP(CP_UTF8);
-	SetConsoleOutputCP(CP_UTF8);
-
-	setvbuf(stdout, nullptr, _IOFBF, 1000);
-
-	try {
-		std::string connectionString =
-			"host=localhost "
-			"port=5432 "
-			"dbname=Books "
-			"user=postgres "
-			"password=L29samolet";
-
-		std::unique_ptr<Wt::Dbo::backend::Postgres> connection = std::make_unique<Wt::Dbo::backend::Postgres>(connectionString);
-
-		Wt::Dbo::Session s;
-		s.setConnection(move(connection));
-
-		s.mapClass<Publisher>("publisher");
-		s.mapClass<Book>("book");
-		s.mapClass<Shop>("shop");
-		s.mapClass<Stock>("stock");
-		s.mapClass<Sale>("sale");
-
-		s.createTables();
-
-
-	}
-	catch (const std::exception& e) {
-		std::cout << "Exception happened " << e.what() << std::endl;
-	}
+	
 }
